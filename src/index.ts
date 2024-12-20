d7704067ed1c2fc01d718ac2deb0005c3224c88c
Luis Rivera <rluis747@gmail.com>
2024-12-20 14:59:24 -0500

    First

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,18 @@ app.get('/', (req, res) => {
   res.send('Hello from TypeScript with Express!');
 });
 
+app.get('/efficiency/data', (req, res) => {
+  try {
+    const { query } = req
+    const { siteId , equipmentType ,  date } = query
+    console.log(siteId, equipmentType, date);
+  } catch (error) {
+    console.log(error);
+    
+  }
+}
+)
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
